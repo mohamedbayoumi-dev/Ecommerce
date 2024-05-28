5d88726da134eef2731481a3fb902bcee52d87f1
Mohamed Bayoumi <mohamedelsayed10zm@gmail.com>
2024-05-28 04:11:24 +0300

    deploy phase one

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@
 import mongoose from "mongoose"
 
 export const dbConnection = async () => {
-  return await mongoose.connect(process.env.CONNECTION_DB_URL)
+  return await mongoose.connect(process.env.CONNECTION_DB_CLOUD)
     .then((res) => console.log("Db Connection Success .."))
     .catch((error) => console.log("Db Connection Fail", error))
 }
