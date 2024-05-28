9c37f6feda502cb80aa0eee51cac5246045374fe
Mohamed Bayoumi <mohamedelsayed10zm@gmail.com>
2024-05-28 03:58:54 +0300

    deploy phase one

--- src/modules/Products/product.routes.js
+++ src/modules/Products/product.routes.js
@@ -1,8 +1,8 @@
 
-import { validationFunction } from '../../middlewares/validation.js';
 import { allowedExtensions } from '../../utils/allowedExtensions.js';
 import { multerFunction } from '../../services/multerCloudinary.js';
 import { asyncHandler } from '../../utils/errorHandling.js'
+import { validationFunction } from '../../middlewares/validation.js';
 import * as validators from './product.validationSchemas.js'
 import { productApisRoles } from './product.endPoints.js';
 import { isAuth } from '../../middlewares/auth.js';
@@ -11,12 +11,12 @@ import Router from 'express'
 const router = Router()
 
 
-router.post('/',isAuth(productApisRoles.CREATE_PRODUCT), multerFunction(allowedExtensions.Images).array('image',2),
+router.post('/', isAuth(productApisRoles.CREATE_PRODUCT), multerFunction(allowedExtensions.Images).array('image', 2),
   validationFunction(validators.addProductSchema),
   asyncHandler(pc.createProduct)
 )
 
-router.put('/',isAuth(), multerFunction(allowedExtensions.Images).array('image'),
+router.put('/', isAuth(), multerFunction(allowedExtensions.Images).array('image'),
   validationFunction(validators.updateProductSchema),
   asyncHandler(pc.updateProduct)
 )
@@ -25,7 +25,7 @@ router.get('/', asyncHandler(pc.getAllProduct))
 router.get('/title', asyncHandler(pc.getAllProductsByTitle))
 router.get('/', asyncHandler(pc.listProducts))
 
-router.delete('/',isAuth(), asyncHandler(pc.deletedProduct))
+router.delete('/', isAuth(), asyncHandler(pc.deletedProduct))
 
 
 export default router;
